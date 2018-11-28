41887afe26e5c80ed989ed61c6bfed34efbaacbf
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-11-28 01:38:20 -0300

    add bootstrap dependency and navbar layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AlphaNavbar from "./component/AlphaNavbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AlphaNavbar/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
---
+++ src/component/AlphaNavbar.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap"
+
+class AlphaNavbar extends Component {
+  render() {
+    return (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Alpha-Project</a>
+          </Navbar.Brand>
+          <Navbar.Toggle/>
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Dishes
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Portfolio
+            </NavItem>
+          </Nav>
+          <Nav pullRight>
+            <NavDropdown eventKey={3} title="User" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Profile</MenuItem>
+              <MenuItem eventKey={3.2}>Settings</MenuItem>
+              <MenuItem divider/>
+              <MenuItem eventKey={3.3}>Log out</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  };
+}
+
+export default AlphaNavbar;
